f2f3aaada01fa34ca7f33abc7fc44902e177652a
sahar omer <somer1@my.yearupunited.org>
2026-04-24 14:46:23 -0400

    main menu is up and working

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,25 +1,44 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import com.pluralsight.ui.Console;
 
 public class Main {
     static void main(){
         ArrayList<Product> inventory = getInventory();
         HashMap<String, Product> inventoryKeyName = getInventoryKeyName(inventory);
-        HashMap<Float, Product> inventoryKeyPrice = getInventoryKeyPrice(inventory);
-        HashMap<String, Product> inventoryKeyDepartment = getInventoryKeyDepartment(inventory);
+       int command;
+       do{
+           command = Console.promptForInt("""
+                   Select an Option:
+                   1: Display Product
+                   2: Display Cart
+                   3: Exit""",1,3);
 
+       }while(command != 3);
 
+        switch (command){
+            case 1:
+                shoppingMenu(inventory,inventoryKeyName);
+                break;
+            case 2:
+                Cart();
+                break;
+            case 3:
+                break;
+            default:
+                System.out.println("invalid input try again!");
+        }
 
 
 
 
     }
+
 
     /**
      * loads the csv file splits it and loads it into a simple arraylist inventory
@@ -55,28 +74,58 @@ public class Main {
         }
         return inventoryWithKey;
     }
+
+    /**
+     * display the list of products the store sells lets customer search filter add to cart and go back to main menu
+     * @param inventory as arraylist for sorting by price and department
+     * @param inventoryKeyName as hashmap for sorting through name
+     */
+    private static void shoppingMenu(ArrayList<Product> inventory, HashMap<String, Product> inventoryKeyName) {
+    }
+
+    /**
+     * displays all the items inside the cart lets user checkout or remove items
+     */
+    private static void Cart() {
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /**
      * get arraylist inventory and turns it into hashmap inventory with department as key
      * @param inventory as arraylist
      * @return inventory as HashMap with department key
      */
-    private static HashMap<String, Product> getInventoryKeyDepartment(ArrayList<Product>  inventory) {
-        HashMap<String,Product> inventoryWithKey = new HashMap<>();
-        for(Product p : inventory){
-            inventoryWithKey.put(p.getDepartment(),p);
-        }
-        return inventoryWithKey;
-    }
-    /**
-     * get arraylist inventory and turns it into hashmap inventory with price as key
-     * @param inventory as arraylist
-     * @return inventory as HashMap with price key
-     */
-    private static HashMap<Float, Product> getInventoryKeyPrice(ArrayList<Product>  inventory) {
-        HashMap<Float,Product> inventoryWithKey = new HashMap<>();
-        for(Product p : inventory){
-            inventoryWithKey.put(p.getPrice(),p);
-        }
-        return inventoryWithKey;
-    }
+//    private static HashMap<String, Product> getInventoryKeyDepartment(ArrayList<Product>  inventory) {
+//        HashMap<String,Product> inventoryWithKey = new HashMap<>();
+//        for(Product p : inventory){
+//            inventoryWithKey.put(p.getDepartment(),p);
+//        }
+//        return inventoryWithKey;
+//    }
+//    /**
+//     * get arraylist inventory and turns it into hashmap inventory with price as key
+//     * @param inventory as arraylist
+//     * @return inventory as HashMap with price key
+//     */
+//    private static HashMap<Float, Product> getInventoryKeyPrice(ArrayList<Product>  inventory) {
+//        HashMap<Float,Product> inventoryWithKey = new HashMap<>();
+//        for(Product p : inventory){
+//            inventoryWithKey.put(p.getPrice(),p);
+//        }
+//        return inventoryWithKey;
+//    }
 }
